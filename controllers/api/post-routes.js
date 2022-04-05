3fc2a5ab97e93176cceafcb670c0428698b3da5a
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-05 14:04:34 -0700

    midway test on insomnia- new associations added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,20 +1,20 @@
 //ADD LIBRARIES
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Rate } = require('../../models');
 
 //POST TO GET ALL
 router.get('/', (req, res) => {
     Post.findAll({
-            attributes: ['id', 'post_url', 'title', 'created_at'],
-            //ADD THE ORDER PROPERTY SO THE MOST CURRENT POSTS SHOW FIRST
-            order: [
-                ['created_at', 'DESC']
-            ],
-            include: [{
-                model: User,
-                attributes: ['username']
-            }]
-        })
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        //ADD THE ORDER PROPERTY SO THE MOST CURRENT POSTS SHOW FIRST
+        order: [
+            ['created_at', 'DESC']
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
             console.log(err);
@@ -25,15 +25,15 @@ router.get('/', (req, res) => {
 //GET ONE POST
 router.get('/:id', (req, res) => {
     Post.findOne({
-            where: {
-                id: req.params.id
-            },
-            attributes: ['id', 'post_url', 'title', 'created_at'],
-            include: [{
-                model: User,
-                attributes: ['username']
-            }]
-        })
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    })
         .then(dbPostData => {
             if (!dbPostData) {
                 res.status(404).json({ message: 'No post found with this id' });
@@ -50,10 +50,10 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     //EXPECTS{ TITLE:'XYZPOST', PST_URL: 'HTTPS//XYZ.COM/PRESS', USER_ID: 1} 
     Post.create({
-            title: req.body.title,
-            post_url: req.body.post_url,
-            user_id: req.body.user_id
-        })
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
             console.log(err)
@@ -61,14 +61,25 @@ router.post('/', (req, res) => {
         });
 });
 
+//UPRATE BETWEEN ROUTER.POST AND /:ID PUT ROUTES
+//PUT/API/POSTS/UPRATE
+router.put('/uprate', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
+});
+
 router.put('/:id', (req, res) => {
     Post.update({
-            title: req.body.title
-        }, {
-            where: {
-                id: req.params.id
-            }
-        })
+        title: req.body.title
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
         .then(dbPostData => {
             if (!dbPostData) {
                 res.status(404).json({ message: 'No post found with this id' });
@@ -84,10 +95,10 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
+        where: {
+            id: req.params.id
+        }
+    })
         .then(dbPostData => {
             if (!dbPostData) {
                 res.status(404).json({ message: 'No post found with this id' });
