72d63abef4af6cff98743471a6678998c9d88ffd
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-04-06 05:42:35 -0700

    handlebars brokering homepage and home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,45 @@
 //SET-UP THE MAIN HOMEPAGE ROUTE - NOW- BECAUSE OF SPECIFYING A TEMPLATE ENGINE LIKE HANDLEBARS- WE CAN USE RES.RENDER() INSTEAD OF RES.SEND() OR RES.SENDFILE() FOR THE RESPONSE
 const router = require('express').Router();
 
+//IMPORT MODULES AND MODELS-NOTE LEAVE OUT COMMENT MODEL TO ADD IN LATER const { Post, User, Comment } = require('../models');
+const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
+
 router.get('/', (req, res) => {
-    //HOMEPAGE.HANDLEBARS EXTENSION IS IMPLIED
-    res.render('homepage');
+  console.log('=====================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM rate WHERE post.id = rate.post_id)'), 'rate_count']
+    ],
+    include: [
+      //   {
+      //     model: Comment,
+      //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      //     include: {
+      //       model: User,
+      //       attributes: ['username']
+      //     }
+      //   },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      //MAKE AN ARRAY OF THE JAVASCRIPT OBJECTS FOR HANDLEBARS TO USE
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
